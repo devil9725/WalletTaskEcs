7488b522f091cc6f99316edcef994b48a12b2531
Arty <devil.tem@gmail.com>
2023-09-27 23:23:38 +0300

    refactor PlayerPrefBalanceStorageSystem (removed EntityManager use)

--- Assets/Scripts/ECS/Systems/Storages/PlayerPrefBalanceStorageSystem.cs
+++ Assets/Scripts/ECS/Systems/Storages/PlayerPrefBalanceStorageSystem.cs
@@ -16,16 +16,16 @@ namespace Counter.Ecs
         {
             var ecb = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>().CreateCommandBuffer(World.Unmanaged);
 
-            Entities.WithoutBurst().WithAll<LoadBalanceStorageTag>().ForEach((Entity entity, PrefsStorageData prefsBalanceComponent, BalanceData v) =>
+            Entities.WithoutBurst().WithAll<LoadBalanceStorageTag>().ForEach((Entity entity, PrefsStorageData prefsBalanceComponent, ref BalanceData balance) =>
             {
                 var value = PlayerPrefs.GetInt(prefsBalanceComponent.Name.ToString(), 0);
                 ecb.RemoveComponent<LoadBalanceStorageTag>(entity);
-                EntityManager.SetComponentData(entity, new BalanceData() { Value = value });
+                balance.Value = value;
             }).Run();
 
-            Entities.WithoutBurst().WithAll<DirtyTag>().ForEach((Entity entity, PrefsStorageData prefsBalanceComponent, BalanceData v) =>
+            Entities.WithoutBurst().WithAll<DirtyTag>().ForEach((Entity entity, PrefsStorageData prefsBalanceComponent, BalanceData balance) =>
             {
-                PlayerPrefs.SetInt(prefsBalanceComponent.Name.ToString(), v.Value);
+                PlayerPrefs.SetInt(prefsBalanceComponent.Name.ToString(), balance.Value);
                 ecb.RemoveComponent<DirtyTag>(entity);
             }).Run();            
         }
